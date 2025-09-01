2e704d4043b43e54647a1f14f3df0de93952faf0
sahilgadekar1507 <sahilgadekar15@gmail.com>
2025-09-01 09:34:14 +0530

    added the RTE key to the .env

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -12,7 +12,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey="6zwanr4aezqzo5ukunu4fvwminzcxvxke7z6pvez1r83ubww"
+            apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
